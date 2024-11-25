3ef5cecd2f9367ae958f191f653e0b7d2c6f0af5
Ross Smith <2652064s@student.gla.ac.uk>
2024-11-25 12:02:36 +0000

    feat: Hash reported urls

--- src/routes/battle.ts
+++ src/routes/battle.ts
@@ -1,24 +1,31 @@
 import { Router } from "express";
 import { BattleResult } from "../db/models/BattleResultModel";
 import { verifyAuthToken } from "../util/auth";
+import bcrypt from "bcryptjs";
 
 const POINT_LIMIT = 200;
 
 const BattleRouter = Router();
 
 BattleRouter.post("/reportResult", verifyAuthToken, async (req, res) => {
-  console.log(req.body);
+  const hashedUrl = await bcrypt.hash(req.body.url, 10);
   try {
-    const existingBattle = await BattleResult.findOne({
+    const existingBattles = await BattleResult.find({
       username: req.body.user.username,
-      url: req.body.url,
     });
+    let existingBattle: { [key: string]: any };
+    for (let battle of existingBattles) {
+      if (await bcrypt.compare(req.body.url, battle.url)) {
+        existingBattle = battle;
+        break;
+      }
+    }
     if (existingBattle || (req.body.points && req.body.points > POINT_LIMIT)) {
       return res.status(400).json({ error: "Invalid BattleResult." });
     }
     const newBattle = new BattleResult({
       username: req.body.user.username,
-      url: req.body.url,
+      url: hashedUrl,
       result: req.body.result,
       points: req.body.score || 0,
     });
@@ -53,10 +60,16 @@ BattleRouter.get("/highScore", verifyAuthToken, async (req, res) => {
 
 BattleRouter.post("/getBattleResult", verifyAuthToken, async (req, res) => {
   try {
-    const existingBattle = await BattleResult.findOne({
+    const existingBattles = await BattleResult.find({
       username: req.body.user.username,
-      url: req.body.url,
     });
+    let existingBattle: { [key: string]: any };
+    for (let battle of existingBattles) {
+      if (await bcrypt.compare(req.body.url, battle.url)) {
+        existingBattle = battle;
+        break;
+      }
+    }
     if (!existingBattle) {
       return res.status(200).json({ result: "not_played" });
     }
